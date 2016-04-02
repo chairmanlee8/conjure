d7ffd9744102f0989c9328858e905ee8170ae74a
Andrew Lee <smiley325squared@gmail.com>
2016-04-02 16:53:51 -0500

    Interim commit

--- src/cache.js
+++ src/cache.js
@@ -5,3 +5,32 @@ export default {
     release: release,
     get: get
 }
+
+var DATA = {},          // uuid => object data (whatever Model.loadFromRemote returns)
+    HOLDING = false;
+
+function get (...models) {
+    // First return anything that's cached
+    var missed = [];
+    models.forEach(function (model) {
+        if (model.uuid && DATA.hasOwnProperty(model.uuid)) {
+            model.onLoad(DATA[model.uuid]);
+        } else {
+            missed.push(model);
+        }
+    });
+
+    // Then group the cache misses into classes. We can't use constructor.name since its not guaranteed to exist,
+    // but we can compare constructors to check for class equivalence instead.
+    var bucketHandles = [];
+    var buckets = {};
+    missed.forEach(function (model) {
+        var thisHandle = model.constructor, i;
+        for (i = 0; i < bucketHandles.length; i++) {
+            if (bucketHandles[i] === thisHandle) break;
+        }
+        if (i == bucketHandles.length) {
+            bucketHandles.push(thisHandle);
+        }
+    });
+}
