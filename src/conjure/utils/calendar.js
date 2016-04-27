c43c86601a06d80a4bc59821f330767a7549dd26
Andrew Lee <andrew.lee+garden@avant.com>
2016-04-27 14:08:46 -0500

    Add calendar function to utils.

---
+++ src/conjure/utils/calendar.js
@@ -0,0 +1,77 @@
+export default class Calendar {
+    constructor () {
+        this.grid = [];
+        this.week = 0;
+
+        let today = new Date();
+        set(today.getFullYear(), today.getMonth()+1, today.getDate());
+    }
+
+    setYear  (y) { this.set(y, this.month, this.date) }
+    setMonth (m) { this.set(this.year, m, this.date) }
+    setDate  (d) { this.set(this.year, this.month, d) }
+    setWeek  (w) { this.week = w }
+
+    set (y, m, d) {
+        this.year = y;
+        this.month = m;
+        this.date = d;
+
+        // Set calendar month to the given month and year. (Month and year in natural units)
+        var monthStart = new Date(y, m-1, 1);
+        var monthEnd = new Date(y, m, 0);
+        var calStart = new Date(y, m-1, 1-monthStart.getDay());
+        var calEnd = new Date(y, m, 6-monthEnd.getDay());
+        var base = [calStart.getFullYear(), calStart.getMonth()+1, calStart.getDate()];
+        var wk = 0, wkDay = 0;
+
+        // Clear previous calendar
+        this.grid.length = 0;
+        this.grid.push([]);
+        this.year = y;
+        this.month = m;
+        this.date = d;
+
+        // Previous month
+        for (var i = 0; i < monthStart.getDay(); i++) {
+            this.grid[wk].push([base[0], base[1], base[2] + i]);
+            wkDay++;
+        }
+
+        // Current month
+        base = [monthStart.getFullYear(), monthStart.getMonth()+1, monthStart.getDate()];
+        for (var i = 0; i < monthEnd.getDate(); i++) {
+            this.grid[wk].push([base[0], base[1], base[2] + i]);
+
+            // Is this the week?
+            if (base[2] + i == d) {
+                this.week = wk;
+            }
+
+            if (wkDay++ % 7 == 6) {
+                this.grid.push([]);
+                wk++;
+            }
+        }
+
+        // Next month
+        if (monthEnd.getMonth() == calEnd.getMonth() && monthEnd.getDate() == calEnd.getDate()) {
+            // Check empty last row
+            if (this.grid[this.grid.length-1].length == 0) {
+                this.grid.length -= 1;
+            }
+            return;
+        }
+
+        base = [calEnd.getFullYear(), calEnd.getMonth()+1, 1];
+        var ctr = 0;
+        for (var i = (monthEnd.getDay() + 1) % 7; i <= calEnd.getDay(); i++) {
+            this.grid[wk].push([base[0], base[1], base[2] + (ctr++)]);
+        }
+
+        // Check empty last row
+        if (this.grid[this.grid.length-1].length == 0) {
+            this.grid.length -= 1;
+        }
+    }
+}
